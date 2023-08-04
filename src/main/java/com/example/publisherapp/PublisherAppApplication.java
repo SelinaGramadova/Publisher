14ca0045d1f857ff71f47f6d03af51e273a58dfd
valeri.chakarov <valeri.chakarov@egt-digital.com>
2023-08-04 13:30:09 +0300

    Committing the publisher app

---
+++ src/main/java/com/example/publisherapp/PublisherAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.publisherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherAppApplication.class, args);
+	}
+
+}
